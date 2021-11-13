92de18af214883b9905326fefaeb4ca604d5f813
idreeskhan1991 <idreeskhan1991@gmail.com>
2021-11-13 14:01:04 +0530

    Updated bucket name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = var.s3
+    bucket = "cicd-jen-ter-buck"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
--- variables.tf
+++ variables.tf
@@ -11,8 +11,3 @@ variable "ami" {
   description = "Base AMI to launch the instances"
   default = "ami-0416f96ae3d1a3f29"
 }
-
-variable "s3" {
-  description = "S3 Bucket to store terraform state"
-  default = "cicd-jen-ter-buck"
-}
