0b2056cfa07c08114db12d34e2bbe7d121db6c3d
idreeskhan1991 <92720694+idreeskhan1991@users.noreply.github.com>
2021-11-13 13:31:32 +0530

    updated bucket name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "cicd-jen-ter-buck"
+    bucket = var.s3
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
